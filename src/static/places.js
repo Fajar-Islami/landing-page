dee636832cf0bffbc1656dee8174d0f61a54ddb8
Fajar-islami <ahmadfajarislami@protonmail.com>
2020-12-13 16:43:00 +0700

    upload

---
+++ src/static/places.js
@@ -0,0 +1,16 @@
+const places = [
+  {
+    title: "Green",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium sunt dolorum ducimus sit, sed eveniet earum vitae dolorem qui odio hic cupiditate molestias quos nisi, magnam voluptate veniam. Cum, praesentium.",
+    imageUrl: process.env.PUBLIC_URL + "/assets/island1.jpg",
+    time: "1500",
+  },
+  {
+    title: "Island 2",
+    description: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium sunt dolorum ducimus sit, sed eveniet earum vitae dolorem qui odio hic cupiditate molestias quos nisi, magnam voluptate veniam. Cum, praesentium.",
+    imageUrl: process.env.PUBLIC_URL + "/assets/island2.jpg",
+    time: "1500",
+  },
+];
+
+export default places;
